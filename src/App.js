24af2b309050e2391cb2d8687f8e65e5c13b3820
Ziye <51488705+ZZtheNoob@users.noreply.github.com>
2022-08-24 22:29:20 -0700

    dark mode

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import Post from "./components/Post";
 import cn from "classnames";
+import Checkbox from "@mui/material/Checkbox";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
 
 function App() {
   const DEFAULT_POST_NUMBER = 12;
@@ -10,6 +13,7 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [isStarred, setIsStarred] = useState(false);
   const [postNum, setPostNum] = useState(DEFAULT_POST_NUMBER);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const handleLatest = () => {
     setIsStarred(false);
@@ -20,8 +24,12 @@ function App() {
   };
 
   const handleShowMore = () => {
-    setPostNum(postNum + SHOW_MORE_NUMBER)
-  }
+    setPostNum(postNum + SHOW_MORE_NUMBER);
+  };
+
+  const handleMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   useEffect(() => {
     async function getLatestStories() {
@@ -49,55 +57,84 @@ function App() {
   }, [isStarred, postNum]);
 
   return (
-    <div className="App">
-      <div className="main">
-        <div className="header">
-          <h2 className="orangeText">Y</h2>
-          <h2 className="title">Hacker News</h2>
+    <div className={cn("container", { "container-dark": isDarkMode })}>
+      <div className={cn("App", { "App-dark": isDarkMode })}>
+        <div className="main">
+          <div className="header">
+            <h2 className="orangeText">Y</h2>
+            <h2 className="title">Hacker News</h2>
+            <span className="buttonContainer">
+              <button
+                className={cn("button", {
+                  "button-starred": !isStarred,
+                  "button-dark": isDarkMode,
+                })}
+                onClick={handleLatest}
+              >
+                latest
+              </button>
+              |
+              <button
+                className={cn("button", {
+                  "button-starred": isStarred,
+                  "button-dark": isDarkMode,
+                })}
+                onClick={handleStar}
+              >
+                starred
+              </button>
+            </span>
+            <div className="darkModeButton">
+              <Checkbox
+                icon={<DarkModeIcon />}
+                checkedIcon={<LightModeIcon />}
+                onClick={handleMode}
+                sx={{
+                  "& .MuiSvgIcon-root": { fontSize: 30 },
+                  "&.Mui-checked": {
+                    color: "white",
+                  },
+                }}
+              />
+            </div>
+          </div>
+
+          <ol className="posts">
+            {posts.map((post) => (
+              <Post posts={post} isStarred={isStarred} />
+            ))}
+          </ol>
+
+          <button className="showMore" onClick={handleShowMore}>
+            show more
+          </button>
+        </div>
+
+        <div className="footer">
+          <hr color="#FE7139"></hr>
+          <h4>Hacker News</h4>
           <span className="buttonContainer">
             <button
-              className={cn("button", { "button-starred": !isStarred })}
+              className={cn("button", {
+                "button-starred": !isStarred,
+                "button-dark": isDarkMode,
+              })}
               onClick={handleLatest}
             >
               latest
             </button>
             |
             <button
-              className={cn("button", { "button-starred": isStarred })}
+              className={cn("button", {
+                "button-starred": isStarred,
+                "button-dark": isDarkMode,
+              })}
               onClick={handleStar}
             >
               starred
             </button>
           </span>
         </div>
-
-        <ol className="posts">
-          {posts.map((post) => (
-            <Post posts={post} isStarred={isStarred} />
-          ))}
-        </ol>
-
-        <button className="showMore" onClick={handleShowMore}>show more</button>
-      </div>
-
-      <div className="footer">
-        <hr color="#FE7139"></hr>
-        <h4>Hacker News</h4>
-        <span className="buttonContainer">
-          <button
-            className={cn("button", { "button-starred": !isStarred })}
-            onClick={handleLatest}
-          >
-            latest
-          </button>
-          |
-          <button
-            className={cn("button", { "button-starred": isStarred })}
-            onClick={handleStar}
-          >
-            starred
-          </button>
-        </span>
       </div>
     </div>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,15 +2,15 @@ import "./Post.css";
 import Checkbox from "@mui/material/Checkbox";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import StarIcon from "@mui/icons-material/Star";
-import { useState } from 'react';
+import { useState } from "react";
 
 const Post = ({ posts, isStarred }) => {
   const commentNum = posts.kids ? posts.kids.length : 0;
-  const [isSave, setIsSave] = useState(false)
-  var diff = Number(Date.now().toString().slice(0,10)) - posts.time;
-  var d = Math.floor(diff / (3600*24));
-  var h = Math.floor(diff % (3600*24) / 3600);
-  var m = Math.floor(diff % 3600 / 60);
+  const [isSave, setIsSave] = useState(false);
+  var diff = Number(Date.now().toString().slice(0, 10)) - posts.time;
+  var d = Math.floor(diff / (3600 * 24));
+  var h = Math.floor((diff % (3600 * 24)) / 3600);
+  var m = Math.floor((diff % 3600) / 60);
   var s = Math.floor(diff % 60);
 
   const dDisplay = d > 0 ? d + (d === 1 ? " day, " : " days, ") : "";
@@ -18,16 +18,17 @@ const Post = ({ posts, isStarred }) => {
   const mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
   const sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
 
-  if (dDisplay) diff = dDisplay
+  if (dDisplay) diff = dDisplay;
   else if (hDisplay) diff = hDisplay;
   else if (mDisplay) diff = mDisplay;
   else if (sDisplay) diff = sDisplay;
 
   const handleSave = () => {
     setIsSave(!isSave);
-  }
+  };
 
-  var isRender = (isStarred && isSave) || (!isStarred && isSave) || (!isStarred && !isSave)
+  var isRender =
+    (isStarred && isSave) || (!isStarred && isSave) || (!isStarred && !isSave);
 
   return (
     <>
@@ -44,13 +45,14 @@ const Post = ({ posts, isStarred }) => {
               checkedIcon={<StarIcon />}
               onClick={handleSave}
               sx={{
+                color: "#8e8f92",
                 "& .MuiSvgIcon-root": { fontSize: 12 },
                 "&.Mui-checked": {
-                  color: '#FE7139',
+                  color: "#FE7139",
                 },
               }}
             />
-            {isSave ? 'saved' : 'save'}
+            {isSave ? "saved" : "save"}
           </div>
         </div>
       )}
